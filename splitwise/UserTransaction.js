29fb3011586a8fa1449ec4a9b058c3e6d208a93f
Atish Raina <atish.raina91@gmail.com>
2023-10-23 17:35:24 +0530

    add user transaction model

---
+++ splitwise/UserTransaction.js
@@ -0,0 +1,54 @@
+class UserTransaction {
+    #id;
+    #userId;
+    #transactionId;
+    #transactionType;
+    #amount;
+    constructor() {
+        this.#id = 1;
+    }
+
+    set UserId(userId) {
+        this.#userId = userId;
+    }
+
+    set TransactionId(transactionId) {
+        this.#transactionId = transactionId;
+    }
+
+    set TransactionType(transactionType) {
+        this.#transactionType = transactionType;
+    }
+
+    set Amount(amount) {
+        this.#amount = amount;
+    }
+
+    get UserId() {
+        return this.#userId;
+    }
+
+    get TransactionId() {
+        return this.#transactionId;
+    }
+
+    get TransactionType() {
+        return this.#transactionType;
+    }
+    
+    get Amount() {
+        return this.#amount;
+    }
+
+    static createUserTransaction(userId, transactionId, transactionType, amount) {
+        let userTransaction = new UserTransaction();
+        userTransaction.UserId = userId;
+        userTransaction.TransactionId = transactionId;
+        userTransaction.TransactionType = transactionType;
+        userTransaction.Amount = amount;
+        return userTransaction;
+    }
+}
+
+let userTransaction1 = UserTransaction.createUserTransaction(1, 2, 3, 4);
+console.log(userTransaction1.TransactionId);
