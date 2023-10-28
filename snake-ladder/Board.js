cc6b6b62a9a8ef46b8a9eae33714fbc3916f8d04
atish <atish.raina91@gmail.com>
2023-10-28 20:30:58 +0530

    Add code for board

--- snake-ladder/Board.js
+++ snake-ladder/Board.js
@@ -4,8 +4,39 @@ import Player from './Player.js';
 
 class Board {
     #size;
-    #snake;
-    constructor () {
-        this.#size = 100;
+    #snakes;
+    #ladders;
+    #players;
+
+    constructor (size) {
+        this.#size = size;
+    }
+
+    set Snakes(snakes) {
+        this.#snakes = snakes;
+    }
+
+    set Ladders(ladders) {
+        this.#ladders = ladders;
+    }
+
+    set Players(players) {
+        this.#players = players;
+    }
+
+    get Size() {
+        return this.#size;
+    }
+
+    get Snakes() {
+        return this.#snakes;
+    }
+
+    get Ladders() {
+        return this.#ladders;
+    }
+
+    get Players() {
+        return this.#players;
     }
 }
